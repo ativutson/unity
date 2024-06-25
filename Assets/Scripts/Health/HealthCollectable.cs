54aa3379a9c421f0138f6347c77711f661c62b6e
Maddie Caccese <maddiecaccese@gmail.com>
2024-06-24 22:14:33 -0400

    work on sound for health, cannot hear it

--- Assets/Scripts/Health/HealthCollectable.cs
+++ Assets/Scripts/Health/HealthCollectable.cs
@@ -11,9 +11,22 @@ public class HealthCollectable : MonoBehaviour
             HealthCollector hc = c.gameObject.GetComponent<HealthCollector>();
             if(hc != null){
                 hc.ReceiveHealth();
-                AudioSource.PlayClipAtPoint(clip, transform.position);
+                AudioSource.PlayClipAtPoint(clip, c.transform.position);
                 Destroy(this.gameObject);
             }
         }
+    }
+
+    // copies audiosource properties to temp audiosource for playing at a position
+    public static AudioSource PlayClipAtPoint(AudioClip audioSource, Vector3 pos)
+    {
+        GameObject tempGO = new GameObject("TempAudio"); // create the temp object
+        tempGO.transform.position = pos; // set its position
+        AudioSource tempASource = tempGO.AddComponent<AudioSource>(); // add an audio source
+        tempASource.clip = audioSource;
+        tempASource.volume = 0.75f;
+        tempASource.Play(); // start the sound
+        MonoBehaviour.Destroy(tempGO, tempASource.clip.length); // destroy object after clip duration (this will not account for whether it is set to loop)
+        return tempASource; // return the AudioSource reference
     }
 }
